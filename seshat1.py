ecf41eac661b2063a6c9b8784bff7584413f8655
atrexler <adam.trexler@posteo.net>
2017-10-12 21:57:58 -0400

    171012

--- seshat1.py
+++ seshat1.py
@@ -10,9 +10,17 @@ import hashlib
 import Crypto
 from Crypto.Cipher import AES
 from Crypto import Random
+import pymongo
+import ConfigParser
 
-CRYPTO_PATH = '/home/adam/scripts/seshat/crypto/'
-SALT_PATH = '/home/adam/codebase/'   
+CONFIG_PATH = '/home/adam/scripts/seshat/config.txt'
+config = ConfigParser.ConfigParser()
+config.readfp(open(CONFIG_PATH))
+
+CRYPTO_PATH = config.get('seshat1 config','crypto_path')
+SALT_PATH = config.get('seshat1 config','salt_path')
+CRYPTO_NAME = config.get('seshat1 config','crypto_file')
+
 def decryptor(filename,keypass,iv):
     chunk_size = 8192
     cryptor = AES.new(keypass,AES.MODE_CFB,iv)
@@ -31,7 +39,7 @@ key = hashlib.sha256(password).digest()
 with open(SALT_PATH+'seshat_salts.txt','r') as fid:
     iv=fid.read()   
 iv=iv.strip()
-token=decryptor(CRYPTO_PATH+'newfile.txt.enc',key,iv)  
+token=decryptor(CRYPTO_PATH+CRYPTO_NAME,key,iv)  
 token=token.strip()  
 
 
@@ -41,13 +49,37 @@ me = client.users.me()
 workspace_id = me['workspaces'][0]['id']
 
 projects = client.projects.find_by_workspace(workspace_id,iterator_type=None)
+print 'welcome', me['name']
 
+all_tasks=[]
 for project in projects:
     print project['name']
     project_id = project['id']
     project_tasks = client.tasks.find_by_project(project_id,iterator_type=None)
     
     for task in project_tasks:
-        task_id=task['id']
+        all_tasks.append(task['id'])
+
+#check all_tasks against DB 
+#get info and make DB entry for new tasks.
+
+#read 
+for task in all_tasks:
+    client.tasks.find_by_id(task)
+    
+
+
+def newTask(tid):
+    task_info = client.tasks.find_by_id(tid)
+    task_info['name']
+    task_info['projects']
+    task_info['due_on']
+    task_info['created_at']
+    task_info['completed_at']
+    task_info['completed']
+    task_info['assignee']
+    task_info['type']
+    
+    
     
 
