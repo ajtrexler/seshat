3fe588ee3602bc13a038dc770249fc6640697a76
atrexler <adam.trexler@posteo.net>
2017-10-13 22:30:25 -0400

    171013

--- seshat1.py
+++ seshat1.py
@@ -12,6 +12,8 @@ from Crypto.Cipher import AES
 from Crypto import Random
 import pymongo
 import ConfigParser
+from pymongo import MongoClient
+
 
 CONFIG_PATH = '/home/adam/scripts/seshat/config.txt'
 config = ConfigParser.ConfigParser()
@@ -20,6 +22,13 @@ config.readfp(open(CONFIG_PATH))
 CRYPTO_PATH = config.get('seshat1 config','crypto_path')
 SALT_PATH = config.get('seshat1 config','salt_path')
 CRYPTO_NAME = config.get('seshat1 config','crypto_file')
+DB_PATH = config.get('seshat1 config','mongodb_path')
+dbName = config.get('seshat1 config','mongodb_name')
+
+
+
+
+
 
 def decryptor(filename,keypass,iv):
     chunk_size = 8192
@@ -31,34 +40,35 @@ def decryptor(filename,keypass,iv):
                 break
             return cryptor.decrypt(chunk)
 
-        
+def checkAsana():   
 
-## on start-up.
-password = raw_input('enter passphrase:')
-key = hashlib.sha256(password).digest()
-with open(SALT_PATH+'seshat_salts.txt','r') as fid:
-    iv=fid.read()   
-iv=iv.strip()
-token=decryptor(CRYPTO_PATH+CRYPTO_NAME,key,iv)  
-token=token.strip()  
+    ## on start-up.
+    password = raw_input('enter passphrase:')
+    key = hashlib.sha256(password).digest()
+    with open(SALT_PATH+'seshat_salts.txt','r') as fid:
+        iv=fid.read()   
+    iv=iv.strip()
+    token=decryptor(CRYPTO_PATH+CRYPTO_NAME,key,iv)  
+    token=token.strip()  
 
 
-client = asana.Client.access_token(str(token))
-me = client.users.me()
+    client = asana.Client.access_token(str(token))
+    me = client.users.me()
 
-workspace_id = me['workspaces'][0]['id']
-
-projects = client.projects.find_by_workspace(workspace_id,iterator_type=None)
-print 'welcome', me['name']
-
-all_tasks=[]
-for project in projects:
-    print project['name']
-    project_id = project['id']
-    project_tasks = client.tasks.find_by_project(project_id,iterator_type=None)
+    workspace_id = me['workspaces'][0]['id']
     
-    for task in project_tasks:
-        all_tasks.append(task['id'])
+    projects = client.projects.find_by_workspace(workspace_id,iterator_type=None)
+    print 'welcome', me['name']
+    
+    all_tasks=[]
+    for project in projects:
+        print project['name']
+        project_id = project['id']
+        project_tasks = client.tasks.find_by_project(project_id,iterator_type=None)
+        
+        for task in project_tasks:
+            all_tasks.append(task['id'])
+    return client, all_tasks
 
 #check all_tasks against DB 
 #get info and make DB entry for new tasks.
@@ -69,17 +79,53 @@ for task in all_tasks:
     
 
 
-def newTask(tid):
-    task_info = client.tasks.find_by_id(tid)
-    task_info['name']
-    task_info['projects']
-    task_info['due_on']
-    task_info['created_at']
-    task_info['completed_at']
-    task_info['completed']
-    task_info['assignee']
-    task_info['type']
+def newTask(tid,sysflag,db,task_info=None):
+    newDoc={}
+    if sysflag=='asana':
+        task_info = client.tasks.find_by_id(tid)
+        newDoc['type']='asana'
+    elif sysflag=='idea':
+        newDoc['type']='idea'
+    elif sysflag=='todo':
+        newDoc['type']=='todo'
+        
+    newDoc['_id']=tid
+    newDoc['name']=task_info['name']
+    newDoc['projects']=task_info['projects']
+    newDoc['due_on']=task_info['due_on']
+    newDoc['created_at']=task_info['created_at']
+    newDoc['completed_at']=task_info['completed_at']
+    newDoc['completed']=task_info['completed']
+    newDoc['assignee']=task_info['assignee']
     
+    masterTasks = db.masterTasks
     
+    masterTasks.insert_one(newDoc)
     
+    #return flag or log msg for successful DB entry.
 
+def checkDB(db,tasks,sysflag,task_info=None):
+    
+    for t in tasks:
+        if masterTasks.find_one({'_id':t}):
+            continue
+        elif task_info==None:
+            newTask(t,sysflag,db)
+            
+            
+    
+    
+    
+  #whe ncall new goal or idea, pass tid as random or unix timestamp.  
+def main():
+    
+    client, all_tasks = checkAsana()
+    #fire up mongodb.
+    themongo = MongoClient()
+    db = themongo[dbName]
+    
+    checkDB(db,all_tasks,'asana')
+    
+    
+    
+    
